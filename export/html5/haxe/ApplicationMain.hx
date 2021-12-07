b08cbd08fc4d8bbfcbfef5fe954ed25fc4ee07ab
gharry <rlucbouchard@gmail.com>
2021-12-07 12:59:07 -0600

    add missing graphics

--- export/html5/haxe/ApplicationMain.hx
+++ export/html5/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "876";
+		app.meta["build"] = "927";
 		app.meta["company"] = "Luc";
 		app.meta["file"] = "ProjectSalutations";
 		app.meta["name"] = "ProjectSalutations";
