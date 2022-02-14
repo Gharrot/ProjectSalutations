c13ba5c6daf46a06e78610846350e3b00881db0e
gharry <rlucbouchard@gmail.com>
2022-02-14 00:06:00 -0600

    Pause Menu Volume Control Layout
    Not functional yet

--- export/neko/haxe/ApplicationMain.hx
+++ export/neko/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "1106";
+		app.meta["build"] = "1120";
 		app.meta["company"] = "Luc";
 		app.meta["file"] = "ProjectSalutations";
 		app.meta["name"] = "ProjectSalutations";
--- source/PauseMenu.hx
+++ source/PauseMenu.hx
@@ -15,6 +15,16 @@ class PauseMenu extends FlxSubState
 	
 	var title:FlxText;
 	
+	var musicVolText:FlxText;
+	var musicVolAmount:FlxText;
+	var musicVolIncreaseButton:FlxButton;
+	var musicVolDecreaseButton:FlxButton;
+	
+	var sfxVolText:FlxText;
+	var sfxVolAmount:FlxText;
+	var sfxVolIncreaseButton:FlxButton;
+	var sfxVolDecreaseButton:FlxButton;
+	
 	var tutorialButton:FlxButton;
 	var saveAndQuitButton:FlxButton;
 	var unpauseButton:FlxButton;
@@ -37,6 +47,97 @@ class PauseMenu extends FlxSubState
 		title.screenCenter();
 		title.y = 70;
 		
+		setupVolumeControls();
+		setupButtons();
+	}
+	
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+		
+		if (FlxG.keys.justPressed.ESCAPE || FlxG.mouse.justReleasedRight)
+		{
+			returnToGame();
+		}
+	}
+	
+	public function setupVolumeControls()
+	{
+		musicVolText = new flixel.text.FlxText(26, 0, 0, "Music Volume", 26);
+		musicVolText.color = 0xFF000000;
+		musicVolText.alignment = FlxTextAlign.CENTER;
+		add(musicVolText);
+		musicVolText.screenCenter();
+		musicVolText.y = 154;
+		
+		musicVolAmount = new flixel.text.FlxText(26, 0, 0, "100", 22);
+		musicVolAmount.color = 0xFF000000;
+		musicVolAmount.alignment = FlxTextAlign.CENTER;
+		add(musicVolAmount);
+		musicVolAmount.screenCenter();
+		musicVolAmount.y = 200;
+		
+		musicVolIncreaseButton = new FlxButton(272, 199);
+		musicVolIncreaseButton.onDown.callback = modifyMusicVolumeLevel.bind(5);
+		musicVolIncreaseButton.loadGraphic("assets/images/PlusButton.png", true, 32, 32);
+		musicVolIncreaseButton.screenCenter(FlxAxes.X);
+		musicVolIncreaseButton.x += 50;
+		musicVolIncreaseButton.updateHitbox();
+		add(musicVolIncreaseButton);
+		
+		musicVolDecreaseButton = new FlxButton(272, 199);
+		musicVolDecreaseButton.onDown.callback = modifyMusicVolumeLevel.bind(-5);
+		musicVolDecreaseButton.loadGraphic("assets/images/MinusButton.png", true, 32, 32);
+		musicVolDecreaseButton.screenCenter(FlxAxes.X);
+		musicVolDecreaseButton.x -= 50;
+		musicVolDecreaseButton.updateHitbox();
+		add(musicVolDecreaseButton);
+		
+		var sfxOffset:Int = 100;
+		
+		sfxVolText = new flixel.text.FlxText(26, 0, 0, "SFX Volume", 26);
+		sfxVolText.color = 0xFF000000;
+		sfxVolText.alignment = FlxTextAlign.CENTER;
+		add(sfxVolText);
+		sfxVolText.screenCenter();
+		sfxVolText.y = 154 + sfxOffset;
+		
+		sfxVolAmount = new flixel.text.FlxText(26, 0, 0, "100", 22);
+		sfxVolAmount.color = 0xFF000000;
+		sfxVolAmount.alignment = FlxTextAlign.CENTER;
+		add(sfxVolAmount);
+		sfxVolAmount.screenCenter();
+		sfxVolAmount.y = 200 + sfxOffset;
+		
+		sfxVolIncreaseButton = new FlxButton(272, 199 + sfxOffset);
+		sfxVolIncreaseButton.onDown.callback = modifySFXVolumeLevel.bind(5);
+		sfxVolIncreaseButton.loadGraphic("assets/images/PlusButton.png", true, 32, 32);
+		sfxVolIncreaseButton.screenCenter(FlxAxes.X);
+		sfxVolIncreaseButton.x += 50;
+		sfxVolIncreaseButton.updateHitbox();
+		add(sfxVolIncreaseButton);
+		
+		sfxVolDecreaseButton = new FlxButton(272, 199 + sfxOffset);
+		sfxVolDecreaseButton.onDown.callback = modifySFXVolumeLevel.bind(-5);
+		sfxVolDecreaseButton.loadGraphic("assets/images/MinusButton.png", true, 32, 32);
+		sfxVolDecreaseButton.screenCenter(FlxAxes.X);
+		sfxVolDecreaseButton.x -= 50;
+		sfxVolDecreaseButton.updateHitbox();
+		add(sfxVolDecreaseButton);
+	}
+	
+	public function modifyMusicVolumeLevel(amount:Int)
+	{
+		
+	}
+	
+	public function modifySFXVolumeLevel(amount:Int)
+	{
+		
+	}
+	
+	public function setupButtons()
+	{
 		tutorialButton = new FlxButton(370, 415, "Tutorial", viewTutorial);
 		tutorialButton.loadGraphic("assets/images/DenButton.png", true, 160, 56);
 		ButtonUtils.fixButtonText(tutorialButton, 24, 9, 1);
@@ -44,9 +145,9 @@ class PauseMenu extends FlxSubState
 		tutorialButton.x += 90;
 		add(tutorialButton);
 		
-		saveAndQuitButton = new FlxButton(370, 415, "Save & Quit", saveAndQuit);
+		saveAndQuitButton = new FlxButton(370, 415, "Save/Quit", saveAndQuit);
 		saveAndQuitButton.loadGraphic("assets/images/DenButton.png", true, 160, 56);
-		ButtonUtils.fixButtonText(saveAndQuitButton, 20, 12, 1);
+		ButtonUtils.fixButtonText(saveAndQuitButton, 24, 10, 1);
 		saveAndQuitButton.screenCenter(FlxAxes.X);
 		saveAndQuitButton.x -= 90;
 		add(saveAndQuitButton);
@@ -75,16 +176,6 @@ class PauseMenu extends FlxSubState
         add(infiniteFoodToggle);
 		
 		updateInfiniteFoodToggle();
-	}
-	
-	override public function update(elapsed:Float)
-	{
-		super.update(elapsed);
-		
-		if (FlxG.keys.justPressed.ESCAPE || FlxG.mouse.justReleasedRight)
-		{
-			returnToGame();
-		}
 	}
 	
 	private function toggleInfiniteFood()
